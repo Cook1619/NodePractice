89867fb667fac634af23adacf41ec5b184a8ce1c
Matt <cook6348@gmail.com>
2019-02-17 19:02:42 -0600

    started styling

--- Routing-with-Express/app.js
+++ Routing-with-Express/app.js
@@ -15,7 +15,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'./', 'views','pageNotFound.html'));
+    res.status(404).sendFile(path.join(__dirname,'./', 'views','pageNotFound.html'));
 })
 
 app.listen(3000);
